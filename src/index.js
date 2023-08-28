6e7d0f1aa2fa88bacf9ee0c4be00f539ecc65b0c
MartaDorokhova <112471191+MartaDorokhova@users.noreply.github.com>
2023-08-28 13:41:41 +0300

    Реализовать основной компонент блога с роутингом

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { Blog } from './App';
+import { Blog } from './blog';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
+	<BrowserRouter>
 		<Blog />
-	</React.StrictMode>,
+	</BrowserRouter>,
 );
