3b43875807b781a95dc057829304148e60e9ac23
MartaDorokhova <112471191+MartaDorokhova@users.noreply.github.com>
2023-08-29 17:06:37 +0300

    Реализовать шапку

--- src/blog.js
+++ src/blog.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { Header } from './components';
 import styled from 'styled-components';
 
 const Content = styled.div`
@@ -7,13 +8,21 @@ const Content = styled.div`
 const H2 = styled.h2`
 	text-align: center;
 `;
+const AppColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 1000px;
+	min-height: 100%;
+	background-color: #fff;
+	margin: 0 auto;
+`;
 
-const Header = () => <div>Шапка</div>;
 const Footer = () => <div>Футер</div>;
 
 export const Blog = () => {
 	return (
-		<>
+		<AppColumn>
 			<Header />
 			<Content>
 				<H2>Контент страницы</H2>
@@ -29,6 +38,6 @@ export const Blog = () => {
 				</Routes>
 			</Content>
 			<Footer />
-		</>
+		</AppColumn>
 	);
 };
---
+++ src/components/header/header.js
@@ -0,0 +1,30 @@
+import { ControlPanel, Logo } from './components';
+import styled from 'styled-components';
+
+const Discription = styled.div`
+	font-style: italic;
+`;
+
+const HeaderContainer = ({ className }) => (
+	<div className={className}>
+		<Logo />
+		<Discription>
+			Веб-технологии
+			<br />
+			Написание кода <br /> Разбор ошибок
+		</Discription>
+		<ControlPanel />
+	</div>
+);
+
+export const Header = styled(HeaderContainer)`
+	display: flex;
+	justify-content: space-between;
+	position: fixed;
+	top: 0;
+	width: 1000px;
+	height: 120px;
+	padding: 20px 40px;
+	box-shadow: 0px -2px 17px #000;
+	background-color: #fff;
+`;
